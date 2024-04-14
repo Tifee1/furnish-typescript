f2d059705204da85e3c438485560ff64e1d5e16e
Tifee1 <boludami238@gmail.com>
2024-04-14 07:04:57 +0100

    image issues 02

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com', 'course-api.com'],
+    domains: ['res.cloudinary.com', 'www.course-api.com', 'course-api.com'],
   },
 }
 
